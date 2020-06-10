9e586aca3b66a204f73529dd356cc0302be43054
mfrancia <matteo.francia2@studio.unibo.it>
2020-06-10 17:15:36 +0200

    add comments

--- src/main/python/charts.py
+++ src/main/python/charts.py
@@ -9,18 +9,18 @@ results = "../../../results/"
 
 # #############################################################################
 # A plot with random data
-df = pd.read_csv(outputs + "geolife.csv")
-fig, ax = plt.subplots(figsize=(8, 4))
-X = df["timestamp"]
-Y = np.random.rand(len(X.index))
-Z = np.random.rand(len(X.index))
-ax.set_xlabel("$Time (s)$")
-ax.set_ylabel("$\\alpha$")
-ax.plot(X, Y, label="Y", marker="s", ls="-")
-ax.plot(X, Z, label="Z", marker="o", ls="--")
-ax.grid(True)
-ax.set_title("A plot with random data")
-ax.legend()
-plt.tight_layout()
-plt.savefig(results + "dummy.pdf")
+df = pd.read_csv(outputs + "geolife.csv") # read data
+fig, ax = plt.subplots(figsize=(8, 4)) # create the figure
+X = df["timestamp"] # X data
+Y = np.random.rand(len(X.index)) # Y data
+Z = np.random.rand(len(X.index)) # Z data
+ax.set_xlabel("Time (s)") # X axis label
+ax.set_ylabel("$\\alpha$") # Y axis label (latex math mode $$)
+ax.plot(X, Y, label="Y", marker="s", ls="-") # Draw a line
+ax.plot(X, Z, label="Z", marker="o", ls="--") # Draw the other line
+ax.grid(True) # Draw the grid
+ax.set_title("A plot with random data") # Set the title
+ax.legend() # Draw the legend
+plt.tight_layout() # Set the layout
+plt.savefig(results + "dummy.pdf") # Save to pdf
 # #############################################################################
