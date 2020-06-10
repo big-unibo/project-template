e6e5e10e5eadd01955be1d65de272b45b80376f9
mfrancia <matteo.francia2@studio.unibo.it>
2020-06-10 09:44:13 +0200

    write to file with no index

--- src/main/python/sample.py
+++ src/main/python/sample.py
@@ -8,4 +8,4 @@ df = pd.read_csv(datasets + "geolife.csv")
 df = df[["userid", "trajectoryid", "latitude", "longitude", "date", "time"]]
 df["latitude"] = np.round(df["latitude"], 8)
 df["longitude"] = np.round(df["longitude"], 8)
-df.to_csv(outputs + "geolife.csv")
+df.to_csv(outputs + "geolife.csv", index=False)
