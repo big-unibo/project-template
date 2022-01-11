982bc23248db9aaf009192701c0efa6df7a59e3e
Matteo Francia <m.francia@unibo.it>
2022-01-11 14:29:09 +0100

    Update sample.py

--- src/main/python/sample.py
+++ src/main/python/sample.py
@@ -15,6 +15,6 @@ df = pd.read_csv(datasets + "geolife.csv")
 df = df[["userid", "trajectoryid", "latitude", "longitude", "date", "time"]]
 df["latitude"] = np.round(df["latitude"], 8)
 df["longitude"] = np.round(df["longitude"], 8)
-df["timestamp"] = (df['date'].astype(str) + " " + df['time'].astype(str)).apply(lambda x: time.mktime(datetime.strptime(x, datetime_format_notz).astimezone(tz).timetuple())).astype(int)
+df["timestamp"] = (df['date'].astype(str) + " " + df['time'].astype(str)).apply(lambda x: datetime.timestamp(datetime.strptime(x, datetime_format_notz).astimezone(tz))).astype(int)
     
 df.to_csv(outputs + "geolife.csv", index=False)
