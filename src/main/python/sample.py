d189c893faa48fbb02b6e2248aa0423c96aa5231
Matteo Francia <m.francia@unibo.it>
2022-02-01 16:52:59 +0100

    Update sample.py

--- src/main/python/sample.py
+++ src/main/python/sample.py
@@ -4,17 +4,14 @@ import time
 from datetime import date, timedelta, datetime
 import pytz
 tz = pytz.timezone('Europe/Rome')
-
 datasets = "../../../datasets/"
 outputs = "../../../outputs/"
-date_format     = '%Y-%m-%d'
+date_format = '%Y-%m-%d'
 datetime_format_notz = '%Y-%m-%d %H:%M:%S'
 datetime_format = '%Y-%m-%d %H:%M:%S %Z'
-
 df = pd.read_csv(datasets + "geolife.csv")
 df = df[["userid", "trajectoryid", "latitude", "longitude", "date", "time"]]
 df["latitude"] = np.round(df["latitude"], 8)
 df["longitude"] = np.round(df["longitude"], 8)
 df["timestamp"] = (df['date'].astype(str) + " " + df['time'].astype(str)).apply(lambda x: datetime.timestamp(datetime.strptime(x, datetime_format_notz).astimezone(tz))).astype(int)
-    
 df.to_csv(outputs + "geolife.csv", index=False)
